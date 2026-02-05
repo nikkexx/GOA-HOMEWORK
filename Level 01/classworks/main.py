2c6f5608eae527ee91fd56682f24ecb4d9c7a7d4
nikkexx <nikanikuradze34@gmail.com>
2026-02-05 14:55:08 +0400

    first commit

---
+++ Level 01/classworks/main.py
@@ -0,0 +1,9 @@
+name = "Nika"        # string (სტრინგი)
+surname = "Nikuradze"  # string (სტრინგი)
+age = 16             # integer (მთელი რიცხვი)
+height = 1.75        # float (ათწილადი რიცხვი)
+
+print(name)
+print(surname)
+print(age)
+print(height)   #
---
+++ Level 01/homeworks/main.py
@@ -0,0 +1,19 @@
+python_info = "Python არის მაღალ დონეზე შექმნილი პროგრამირების ენა. ის მარტივი სინტაქსით გამოირჩევა და გამოიყენება ვებ-დეველოპმენტში, თამაშების შექმნაში, ხელოვნურ ინტელექტში და ასშ."
+
+print(python_info)
+
+
+# print() ფუნქცია Python-ში გამოიყენება ეკრანზე სასურველი ინფორმაციის გამოსატანად
+
+my_age = 15
+friends_age = 15
+
+print(my_age)
+print(friends_age)
+
+my_name = "Nika"
+my_friends_name = "Luka"
+my_friends_name2 = "Gega"
+
+print(my_name + my_friends_name + my_friends_name2)
+
