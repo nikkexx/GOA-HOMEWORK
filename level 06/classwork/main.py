a3e38b2817c8b612c82b5d171094a61fcd9ab792
nikkexx <nikanikuradze34@gmail.com>
2026-02-23 20:09:35 +0400

    second commit

---
+++ level 06/classwork/main.py
@@ -0,0 +1,20 @@
+a= int(input("შეიყვანე პირველი რიცხვი"))
+b = int(input("შეიყვანე მეორე რიცხვი"))
+print("მიმატება:", a + b)
+print("გამოკლება:", a - b)
+print("გამრავლება:", a * b)
+print("გაყოფა ნაშთით:", a / b)
+print("მთელი გაყოფა:", a // b)
+print("ნაშთი:", a % b)
+print("ხარისხში აყვანა:", a ** b)
+
+print(bool(int(True) - int(False) + int(False)))
+
+# Int(True) ნიშნავს Trues- ს გადაიყვანს მთელ რიცხვად ერთად
+# int(False) False გადაიყვან მთელ რიხვად 0 ად
+# საბოლოო პასუხი იქნება True
+
+int("5")
+str(10)
+bool(1)
+bool(0)
