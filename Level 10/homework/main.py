73d2d7f26abd7b2e0edb9669f397f8673d1ad6c8
nikkexx <nikanikuradze34@gmail.com>
2026-03-12 20:48:50 +0400

    second commit

---
+++ Level 10/homework/main.py
@@ -0,0 +1,42 @@
+for i in range(50, 201):
+    print(i)
+
+for i in range(50, 201):
+    print(i)
+
+for i in range(101, 151, 2):
+    print(i)
+
+
+i = 1
+while i <= 50:
+    print(i)
+    i += 1    
+
+
+i = 20
+while i <= 60:
+    print(i)
+    i += 5
+
+surname = input("Enter your surname: ")
+
+for letter in surname:
+    print(letter)
+
+stock = 300
+while stock > 0:
+    stock -= 2
+    print("You have bought the drink")
+    print("Drinks left:", stock)
+
+print("Out of stock")
+
+
+for i in range(50, 19, -3):
+    print(i)
+
+i = 150
+while i >= 0:
+    print(i)
+    i -= 1    
